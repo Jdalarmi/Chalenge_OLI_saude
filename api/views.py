9af80679ee3e49b18ce9a7ba65990d93af68cc64
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-10-18 15:05:38 -0300

    iniciando função de criar

--- api/views.py
+++ api/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import ClientSerializer
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.exceptions import APIException
 
-
-
+@swagger_auto_schema(method='post', request_body=ClientSerializer)
 @api_view(['POST'])
 def criar(request):
-    return Response('ok')
+    """
+    Exemplo da requisição
+    """
+    if request.method == 'GET':
+        raise APIException(403, 'Sem acesso get')
+    
+    serializer = ClientSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
