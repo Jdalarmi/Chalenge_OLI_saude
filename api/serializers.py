3ac9d0ebb471b6c8570e4bb3dd396eaec1c9f94f
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-10-19 15:24:00 -0300

    Criada Endpoint de criar um cliente com doenças

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,27 @@
 from rest_framework import serializers
-
 from api.models import ProblemsHealth, Client
 
+class ProblemHealthSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProblemsHealth
+        fields = '__all__'
+
 class ClientSerializer(serializers.ModelSerializer):
+    problem_health = ProblemHealthSerializer(many=True)  
+
     class Meta:
         model = Client
-        fields = ['problem_health']
+        fields = '__all__'
 
-        
+    def create(self, validated_data):
+        problems_data = validated_data.pop('problem_health')
+        client = Client.objects.create(**validated_data)
+
+        for problem_data in problems_data:
+            problem = ProblemsHealth.objects.create(**problem_data)
+            client.problem_health.add(problem)
+
+        return client
+
+
+
--- api/views.py
+++ api/views.py
@@ -4,18 +4,29 @@ from rest_framework.decorators import api_view
 from .serializers import ClientSerializer
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.exceptions import APIException
+import logging
+from django.http import JsonResponse
+
+logger = logging.getLogger(__name__)
 
 @swagger_auto_schema(method='post', request_body=ClientSerializer)
 @api_view(['POST'])
 def criar(request):
     """
-    Exemplo da requisição
+    Endpoint para criar um novo cliente.
     """
-    if request.method == 'GET':
-        raise APIException(403, 'Sem acesso get')
+    if request.method != 'POST':
+        logger.error('Método não permitido. Apenas requisições POST são aceitas.')
+        return JsonResponse({'error': 'Método não permitido'}, status=405)  # Método não permitido (405)
+
+    logger.debug('Requisição POST recebida com dados: %s', request.data)
     
     serializer = ClientSerializer(data=request.data)
     
     if serializer.is_valid():
+        logger.debug('Dados válidos. Salvando o objeto Client no banco de dados.')
         serializer.save()
-    return Response(serializer.data)
+        return JsonResponse(serializer.data, status=201) 
+    else:
+        logger.error('Dados inválidos: %s', serializer.errors)
+        return JsonResponse(serializer.errors, status=400)  
