fdedc483f182e86b626a89a1efab83e66afb2dac
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-10-24 16:00:59 -0300

    Change swagger information

--- core/urls.py
+++ core/urls.py
@@ -7,12 +7,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Sua API Django REST",
+      title="API - Challenge OLI Saúde",
       default_version='v1',
-      description="Descrição da sua API",
+      description="O Desafio consistia em criar uma API capaz de gerenciar informações de saúde do cliente, fornecendo recursos para criar, editar, visualizar e listar clientes. Além disso, a API precisava calcular o risco de saúde com base nos problemas de saúde relatados pelos clientes, usando a fórmula score = (1 / (1 + e^(-2.8 + sd))) * 100. Esse calculo é baseado em um atributo cujo é o grau da gravidade do problema de saúde do cliente.",
       terms_of_service="https://www.suaapi.com/terms/",
-      contact=openapi.Contact(email="contato@suaapi.com"),
-      license=openapi.License(name="Sua Licença"),
+      contact=openapi.Contact(name='Jeferson Dalarmi',email="dalarmi.jeferson@gmail.com",url='https://github.com/Jdalarmi',),
+      
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
